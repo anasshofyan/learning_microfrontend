5470609a4bdca63ab91ab7fb432b048a10a41c7e
Anas Shofyan Martunis Fateh <shofyananasmf@gmail.com>
2023-12-14 21:24:47 +0700

    feat: add module federation

--- 02 Code/ecomm-boilerplate/products/webpack.config.js
+++ 02 Code/ecomm-boilerplate/products/webpack.config.js
@@ -1,11 +1,20 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
   devServer: {
-    port: 3000,
+    port: 3001,
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: "products",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./ProductsIndex": "./src/index",
+      },
+      shared: ["faker"],
+    }),
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
